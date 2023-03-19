caa755b21012d1a532648d5a2e54c4bcf0519e23
kwannieng <78864242+kwannieng@users.noreply.github.com>
2023-03-19 16:23:34 +0000

    spotify authorization set-up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi()
+
+const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+  }, {});
+}
 
 function App() {
+  const [spotifyToken, setSpotifyToken] = useState("");
+  const [nowPlaying, setNowPlaying] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  useEffect(()=>{
+    console.log("This is what we derived from the URL:", getTokenFromUrl())
+    const spotifyToken = getTokenFromUrl().access_token
+    window.location.hash = "";
+    console.log("This is our Spotify Token", spotifyToken);
+
+    if (spotifyToken){
+      setSpotifyToken(spotifyToken)
+      setLoggedIn(true)
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="http://localhost:8888">Login to Spotify</a>
     </div>
   );
 }
