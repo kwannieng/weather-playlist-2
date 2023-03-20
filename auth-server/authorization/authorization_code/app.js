9a0ba23f251945d5391128dbd3e0d8b2c52e85b3
kwannieng <78864242+kwannieng@users.noreply.github.com>
2023-03-20 22:56:16 +0000

    fixed bug

--- auth-server/authorization/authorization_code/app.js
+++ auth-server/authorization/authorization_code/app.js
@@ -104,13 +104,13 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('localhost:3000/#' +
+        res.redirect('http://localhost:3000/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
           }));
       } else {
-        res.redirect('localhost:3000/#' +
+        res.redirect('http://localhost:3000/#' +
           querystring.stringify({
             error: 'invalid_token'
           }));
