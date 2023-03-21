82ec13baa0d6eeb83686df090887217c01d8b864
kwannieng <78864242+kwannieng@users.noreply.github.com>
2023-03-21 18:45:51 +0000

    completed spotify api

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
 
-const spotifyApi = new SpotifyWebApi()
+const spotifyApi = new SpotifyWebApi();
+
 
 const getTokenFromUrl = () => {
+  console.log(window.location.hash);
   return window.location.hash
     .substring(1)
-    .split('&')
+    .split("&")
     .reduce((initial, item) => {
       let parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
       return initial;
-  }, {});
-}
+    }, {});
+};
 
 function App() {
   const [spotifyToken, setSpotifyToken] = useState("");
@@ -28,14 +30,41 @@ function App() {
 
     if (spotifyToken){
       setSpotifyToken(spotifyToken)
+      spotifyApi.setAccessToken(spotifyToken)
+      spotifyApi.getMe().then((user) => {
+        console.log(user)
+      })
       setLoggedIn(true)
     }
   })
+
+
+  const getNowPlaying = () =>{
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      console.log(response);
+      setNowPlaying({
+        name: response.item.name,
+        albumArt: response.item.album.images[0].url
+      })
+    })  
+}  
   return (
     <div className="App">
-      <a href="http://localhost:8888">Login to Spotify</a>
+      {!loggedIn && <a href="http://localhost:8888">Login to Spotify</a>}
+      {loggedIn && (
+        <>
+          <div>Now Playing: {nowPlaying.name}</div>
+          <div>
+            <img src={nowPlaying.albumArt} style={{height:150}}/>
+          </div>
+        </>
+      )}
+      {loggedIn && (
+          <button onClick={() => getNowPlaying()}>Check Now Playing</button>
+        )}
     </div>
   );
-}
+  }
+
 
 export default App;
