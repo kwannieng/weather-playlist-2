55f50f1dc0d37ee053bbc9ec89acab2408d11060
kwannieng <78864242+kwannieng@users.noreply.github.com>
2023-03-25 18:38:11 +0000

    added hero image effect

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,15 @@
 import React from "react";
-import hero from "../assets/images/hero1.jpg";
 import { Link } from "react-router-dom";
 import "./style.css";
 import Header from "../components/Header";
+import Hero from "../components/hero";
 
 const Home = () =>{
 return <div>
     <Header/>
-    <img src={hero} alt="hero" className="hero"/>
+    <Hero/>
+    {/* <img src={hero} alt="hero" className="hero"/> */}
+
     <div className="intro">
         <h1>Weather Playlist</h1>
         <div>
