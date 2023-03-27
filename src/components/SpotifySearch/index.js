6e7b1a2b71ba9f5808ec843f1970143866ae95b0
kwannieng <78864242+kwannieng@users.noreply.github.com>
2023-03-28 00:42:39 +0100

    spotify searchtrack

--- src/components/SpotifySearch/index.js
+++ src/components/SpotifySearch/index.js
@@ -1,109 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { client_id, client_secret } from '../ID'
+import React, { useEffect, useState } from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
-
-function SpotifyApi() {
-    const spotifyApi = new SpotifyWebApi()
-    const [spotifyToken, setSpotifyToken] = useState('')
-    const [nowPlaying, setNowPlaying] = useState({})
-    const [loggedIn, setLoggedIn] = useState(false)
-  
-    const getTokenFromUrl = () => {
-      console.log(window.location.hash)
-      return window.location.hash
-        .substring(1)
-        .split('&')
-        .reduce((initial, item) => {
-          let parts = item.split('=')
-          initial[parts[0]] = decodeURIComponent(parts[1])
-          return initial
-        }, {})
-    }
-
-    useEffect(() => {
-        console.log('This is what we derived from the URL:', getTokenFromUrl())
-        const spotifyToken = getTokenFromUrl().access_token
-        window.location.hash = ''
-        console.log('This is our Spotify Token', spotifyToken)
-    
-        if (spotifyToken) {
-          setSpotifyToken(spotifyToken)
-          spotifyApi.setAccessToken(spotifyToken)
-          spotifyApi.getMe().then((user) => {
-            console.log(user)
-          })
-          setLoggedIn(true)
-        }
-      })
+import CreatePlaylist from '../PlaylistCreate';
+import DisplayPlaylist from '../PlaylistDisplay';
 
 function TrackSearch() {
-  const [valence, setValence] = useState(localStorage.getItem('valence') || 0.5);
-  const [market, setMarket] = useState(localStorage.getItem('market') || 'US');
-  const [tracks, setTracks] = useState([]);
+const spotifyApi = new SpotifyWebApi;
+const [topArtist, setTopArtist] = useState({});
+const [topTrack, setTopTrack] = useState({});
+const [topGenre, setTopGenre] = useState({});
+const [recommendations, setRecommendation] = useState({});
 
-  useEffect(() => {
-    localStorage.setItem('valence', valence);
-    localStorage.setItem('market', market);
-  }, [valence, market]);
+  const GetTopData = () => {
+   spotifyApi.getMyTopTracks().then(response => {
+      setTopTrack(response.items[0].album.id)
+      console.log(topTrack)
+      localStorage.setItem("Top Track", topTrack)
+    })
+    
+    spotifyApi.getMyTopArtists().then(response => {
+      setTopArtist(response.items[0].id);
+      setTopGenre(response.items[0].genres[0])
+      console.log(topArtist)
+      console.log(topGenre)
+      localStorage.setItem("Top Artist", topArtist)
+      localStorage.setItem("Top Genre", topGenre)
+    })
 
+  }
 
-  const handleSearch = () => {
-    axios.get(`https://api.spotify.com/v1/search?q=genre%3Aambient+valence%3A${valence}&type=track&market=${market}&limit=10`, {
-      headers: {
-        'Authorization': 'Basic ' + (new (client_id + ':' + client_secret).toString('base64'))
-      }
+  const GetTrackList = () => {
+    let seed_artists = localStorage.getItem("Top Artist")
+    let seed_genres = localStorage.getItem("Top Genre")
+    let seed_tracks = localStorage.getItem("Top Track")
+    let target_valence = localStorage.getItem("Valence")
+
+    spotifyApi.getRecommendations({
+      seed_artists: seed_artists,
+      seed_genres: seed_genres,
+      seed_tracks: seed_tracks,
+      target_valence: target_valence
     }).then(response => {
-      setTracks(response.data.tracks.items);
-    }).catch(error => {
-      console.error(error);
-    });
-  };
-
-  const handleCreatePlaylist = () => {
-    const trackUris = tracks.map(track => track.uri);
-
-    axios.post(`https://api.spotify.com/v1/users/${client_id}/playlists`, {
-      name: 'My Playlist',
-      public: false
-    }, {
-      headers: {
-        'Authorization': 'Basic ' + (new (client_id + ':' + client_secret).toString('base64'))
-      }
-    }).then(response => {
-      const playlistId = response.data.id;
-
-      axios.post(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-        uris: trackUris
-      }, {
-        headers: {
-          'Authorization': 'Basic ' + (new (client_id + ':' + client_secret).toString('base64'))
-        }
-      }).then(response => {
-        console.log(response);
-      }).catch(error => {
-        console.error(error);
-      });
-    }).catch(error => {
-      console.error(error);
-    });
-  };
+      // setRecommendation({
+      //   artist: response.tracks[0].artists[0].name,
+      //   track: response.tracks[0].name,
+      //   albumArt: response.tracks[0].album.images[0].url,
+      // })
+      console.log(response)
+    })
+  }
 
   return (
     <div>
-      <div>
-        <label htmlFor="valence">Valence:</label>
-        <input type="range" min="0" max="1" step="0.1" id="valence" value={valence} onChange={handleValenceChange} />
-        <span>{valence}</span>
-      </div>
-      <div>
-        <label htmlFor="market">Market:</label>
-        <input type="text" id="market" value={market} onChange={handleMarketChange} />
-      </div>
-      <button onClick={handleSearch}>Search</button>
-      <button onClick={handleCreatePlaylist}>Create Playlist</button>
+      <button onClick={() => TrackSearch() }>GetData</button>
+      <h1>My Top Artists</h1>
+      <ul>
+       <displayPlaylist/>
+      </ul>
+      <button onClick={() => CreatePlaylist() }>Add Weather Playlist to my Spotify</button>
     </div>
-    
-)}
+  );
 }
+
+
 export default  TrackSearch;
